e10312c76a8bf423a329b0efb9686f60b9154bed
Jase Chiappini <jase25chiappini@gmail.com>
2026-01-23 21:56:30 +1030

    Add PWA register type declarations

---
+++ src/types/pwa-register.d.ts
@@ -0,0 +1,15 @@
+declare module "virtual:pwa-register" {
+  type RegisterSWOptions = {
+    immediate?: boolean;
+    onNeedRefresh?: () => void;
+    onOfflineReady?: () => void;
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
+    onRegisteredSW?: (
+      swUrl: string,
+      registration: ServiceWorkerRegistration | undefined,
+    ) => void;
+    onRegisterError?: (error: Error) => void;
+  };
+
+  export function registerSW(options?: RegisterSWOptions): void;
+}
